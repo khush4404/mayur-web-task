6092d1e01235963a4cc64f0e13dab0ab541e2704
Khush-dbc <khush.dbc@gmail.com>
2025-09-20 16:54:29 +0530

    general changes

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,7 +1,5 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
 // import required modules
 import { Autoplay, Pagination } from 'swiper/modules';
 
